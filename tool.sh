183d85d98e21078322ea70b4db886b62ff505ba5
jianyun.zhao <yihy8023@gmail.com>
2020-03-17 11:54:38 +0800

    更新配置

--- tool.sh
+++ tool.sh
@@ -20,7 +20,7 @@ function clone_source_code() {
   git clone https://github.com/coolsnowwolf/lede $source_path
   cd $source_path || exit 1
   echo 'src-git lienol https://github.com/Lienol/openwrt-package' >>feeds.conf.default
-#  clone_app
+  clone_app
 }
 
 function update_feeds() {
@@ -47,7 +47,7 @@ function make_download() {
 
 function compile_firmware() {
   cd $source_path || exit 1
-  make -j$(nproc) || make -j1 V=s
+  IGNORE_ERRORS=1 make -j$(nproc) || make -j1 V=s
   if [ $? -eq 0 ]; then
     echo "编译完成"
   else
